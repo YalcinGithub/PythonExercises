020395da31921556497a4efb09aa49347dbe6cb9
Yalcin YILDIZ <64442783+yyildizz@users.noreply.github.com>
2021-04-03 14:36:51 +0300

    game2 added

---
+++ loops/guessGame2.py
@@ -0,0 +1,40 @@
+# This is a Guess the Number game.
+import random
+
+guessesTaken = 0
+
+print('Hello! What is your name?')
+myName = input()
+
+number = random.randint(1, 20)
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+
+guess = 0
+
+for guessesTaken in range(7):
+    print('Take a guess.')  # Four spaces in front of "print".
+    guess = input()
+    guess = int(guess)
+
+    if guess < number:
+        print('Your guess is too low.')  # Eight spaces in front of "print"
+
+    if guess > number:
+        print('Your guess is too high.')
+
+    if guess == number:
+        break
+
+if guess == number:
+    guessesTaken = str(guessesTaken + 1)
+    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+    if int(guessesTaken) < 3:
+        print("***")
+    elif int(guessesTaken) < 6:
+        print("**")
+    else : print("*")
+
+if guess != number:
+    number = str(number)
+    print('Nope. The number I was thinking of was ' + number + '.')
+
